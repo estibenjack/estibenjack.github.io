c38535cecf5de43143fa5b5fcafe1bc139e92a5f
Steven <156148848+estibenjack@users.noreply.github.com>
2024-07-03 01:42:18 +0100

    Changed project over to React.js - added navbar, hero section and about section

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about" className="about">
+      <div className="container">
+        <div className="about-container">
+          <div className="about-img-container">
+            <img
+              src="/public/images/about-pic.jpg"
+              alt="steven jackson"
+              className="about-img"
+            />
+          </div>
+          <div className="about-text">
+            <h2 className="about-title">ABOUT ME</h2>
+            <p className="about-desc">
+              Hi, I'm Steven, a self-taught frontend developer based in Belfast,
+              Ireland.
+              <br />
+              <br />
+              With a degree in Spanish and French Studies, I've always had a
+              passion for languages, which extends beyond the spoken word to the
+              language of code.
+              <br />
+              <br />
+              In addition to my fluency in Spanish, French, and Portuguese, I
+              have developed a strong proficiency in HTML, CSS, and JavaScript.
+              My journey into the world of web development was driven by a
+              desire to create visually appealing and user-friendly interfaces.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
