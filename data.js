7677b16788f1d9e14f794d010e397c738d1b3c30
Steven <156148848+estibenjack@users.noreply.github.com>
2024-07-14 03:37:50 +0100

    Added function to change language, tweaked CSS and

---
+++ data.js
@@ -0,0 +1,137 @@
+export const translations = {
+  english: {
+    nav: {
+      projects: 'Projects',
+      about: 'About',
+      contact: 'Contact',
+      selected: 'EN',
+      flag: 'GB',
+    },
+    banner: {
+      hi: "Hi there! I'm",
+      name: 'Steven Jackson',
+      job: 'Frontend Developer',
+    },
+    projectsection: {
+      projectstitle: 'Recent Projects',
+      description1: `This project was build using <b>HTML</b>, <b>CSS</b> and <b>JavaScript</b>. It's a Pokémon themed soundboard. Press the corresponding keys on your keyboard to hear each Pokémon's sound!`,
+      description2: `This project was built using <b>HTML</b>, <b>CSS</b>, and <b>JavaScript</b>. It's an interactive Etch-a-Sketch application. Use your mouse to create drawings on the screen, just like the classic toy!`,
+      description3: `This project was built using <b>ReactJS</b>. It's a travel journal page showing three destinations with photos, descriptions and links. Easily navigate through your entries with a modern, responsive interface!`,
+    },
+    aboutsection: {
+      abouttitle: 'About Me',
+      abouttext: `Hi! I'm Steven, a passionate self-taught developer from Belfast, Ireland.
+            <br />
+            <br />
+            I've always had a passion for languages. I studied Spanish and French at university, and along the way, I picked up Portuguese. Exploring different cultures through languages has been an amazing adventure! Now, I'm super excited to be diving into the world of web development.
+            <br />
+            <br />
+            Lately, I've enjoyed learning frontend web development, sharpening my skills in HTML, CSS, and JavaScript, while also starting my journey with ReactJS. I can't wait to keep learning and see the incredible projects I'll create!`,
+    },
+    contactsection: {
+      contacttitle: 'Contact Me',
+    },
+  },
+  spanish: {
+    nav: {
+      projects: 'Proyectos',
+      about: 'Sobre Mí',
+      contact: 'Contacto',
+      selected: 'ES',
+      flag: 'ES',
+    },
+    banner: {
+      hi: 'Hola! Soy',
+      name: 'Steven Jackson',
+      job: 'Desarrollador Frontend',
+    },
+    projectsection: {
+      projectstitle: 'Proyectos Recientes',
+      description1: `Este proyecto fue construido utilizando <b>HTML</b>, <b>CSS</b> y <b>JavaScript</b>. Es un tablero de sonidos temático de Pokémon. ¡Presiona las teclas correspondientes en tu teclado para escuchar el sonido de cada Pokémon!`,
+      description2: `Este proyecto fue construido utilizando <b>HTML</b>, <b>CSS</b> y <b>JavaScript</b>. Es una aplicación interactiva de Etch-a-Sketch. ¡Usa tu ratón para crear dibujos en la pantalla, como el juguete clásico!`,
+      description3: `Este proyecto fue construido utilizando <b>ReactJS</b>. Es una página de diario de viajes que muestra tres destinos con fotos, descripciones y enlaces. ¡Navega fácilmente por tus entradas con una interfaz moderna y responsiva!`,
+    },
+    aboutsection: {
+      abouttitle: 'Sobre Mí',
+      abouttext: `¡Hola! Soy Steven, un desarrollador apasionado y autodidacta de Belfast, Irlanda.
+            <br />
+            <br />
+            Siempre he tenido una gran pasión por los idiomas. Estudié español y francés en la universidad, y durante ese tiempo, también aprendí portugués. ¡Explorar diferentes culturas a través de los idiomas ha sido una aventura increíble! Ahora, estoy súper emocionado de estar embarcándome en el mundo del desarrollo web.
+            <br />
+            <br />
+            Últimamente, he disfrutado de aprender desarrollo web frontend, perfeccionando mis habilidades en HTML, CSS y JavaScript, mientras empiezo mi aventura con ReactJS. ¡Tengo muchas ganas de seguir aprendiendo!`,
+    },
+    contactsection: {
+      contacttitle: 'Contáctame',
+    },
+  },
+  portuguese: {
+    nav: {
+      projects: 'Projetos',
+      about: 'Sobre Mim',
+      contact: 'Contato',
+      selected: 'PT',
+      flag: 'BR',
+    },
+    banner: {
+      hi: 'Olá! Eu sou',
+      name: 'Steven Jackson',
+      job: 'Desenvolvedor Frontend',
+    },
+    projectsection: {
+      projectstitle: 'Projetos Recentes',
+      description1: `Este projeto foi construído usando <b>HTML</b>, <b>CSS</b> e <b>JavaScript</b>. É uma soundboard temática de Pokémon. Pressione as teclas correspondentes no seu teclado para ouvir o som de cada Pokémon!`,
+      description2: `Este projeto foi construído usando <b>HTML</b>, <b>CSS</b> e <b>JavaScript</b>. É uma aplicação interativa de Etch-a-Sketch. Use o mouse para criar desenhos na tela, como no brinquedo clássico!`,
+      description3: `Este projeto foi construído usando <b>ReactJS</b>. É uma página de diário de viagem mostrando três destinos com fotos, descrições e links. Navegue facilmente pelas suas entradas com uma interface moderna e responsiva!`,
+    },
+    aboutsection: {
+      abouttitle: 'Sobre Mim',
+      abouttext: `Olá! Meu nome é Steven e sou um desenvolvedor apaixonado e autodidata de Belfast, Irlanda.
+                <br />
+                <br />
+                Sempre tive uma profunda paixão pelas línguas. Estudei espanhol e francês na universidade e, durante esse período, também aprendi português. A oportunidade de explorar diferentes culturas através das línguas tem sido enriquecedora. Agora, estou super animado por estar mergulhando no mundo do desenvolvimento web.
+                <br />
+                <br />
+                Recentemente, tenho me divertido aprendendo desenvolvimento web frontend, aprimorando minhas habilidades em HTML, CSS e JavaScript, enquanto começo jornada com ReactJS. Estou ansioso para continuar aprendendo!`,
+    },
+    contactsection: {
+      contacttitle: 'Contate-me',
+    },
+  },
+  french: {
+    nav: {
+      projects: 'Projets',
+      about: 'À propos',
+      contact: 'Contact',
+      selected: 'FR',
+      flag: 'FR',
+    },
+    banner: {
+      hi: 'Salut! Je suis',
+      name: 'Steven Jackson',
+      job: 'Développeur Frontend',
+    },
+    projectsection: {
+      projectstitle: 'Projets Récents',
+      description1:
+        "Ce projet a été réalisé en utilisant <b>HTML</b>, <b>CSS</b> et <b>JavaScript</b>. C'est une table de sons sur le thème de Pokémon. Appuyez sur les touches correspondantes de votre clavier pour entendre le son de chaque Pokémon !",
+      description2:
+        "Ce projet a été réalisé en utilisant <b>HTML</b>, <b>CSS</b> et <b>JavaScript</b>. C'est une application Etch-a-Sketch interactive. Utilisez votre souris pour créer des dessins à l'écran, comme avec le jouet classique !",
+      description3:
+        "Ce projet a été réalisé en utilisant <b>ReactJS</b>. C'est une page de journal de voyage montrant trois destinations avec des photos, des descriptions et des liens. Naviguez facilement dans vos entrées avec une interface moderne et réactive !",
+    },
+    aboutsection: {
+      abouttitle: 'À propos de moi',
+      abouttext: `Salut ! Je m'appelle Steven et je suis un développeur passionné et autodidacte de Belfast, en Irlande.
+                <br />
+                <br />
+                J'ai toujours eu une immense passion pour les langues. J'ai étudié l'espagnol et le français à l'université, et durant cette période, j'ai aussi appris le portugais. La possibilité d'explorer différentes cultures à travers les langues a été enrichissante. Maintenant, je suis super enthousiaste de me plonger dans le monde de la programmation.
+                <br />
+                <br />
+                Dernièrement, j'ai pris plaisir à apprendre développement web frontend, en perfectionnant compétences en HTML, CSS et JavaScript, tout en commençant mon aventure avec ReactJS. J'ai vraiment hâte de poursuivre mon apprentissage !`,
+    },
+    contactsection: {
+      contacttitle: 'Contactez-moi',
+    },
+  },
+};
--- index.js
+++ index.js
@@ -15,41 +15,108 @@ function hideNav() {
   mobileNav.classList.remove('is-active');
 }
 
+// --- change language --- //
+
+import { translations } from './data.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const langOptions = document.querySelectorAll('.lang-options ul li');
+  langOptions.forEach(option => {
+    option.addEventListener('click', event => {
+      event.preventDefault();
+      const selectedLang = event.currentTarget
+        .querySelector('a')
+        .getAttribute('data-lang');
+      changeLanguage(selectedLang);
+    });
+  });
+});
+
+function changeLanguage(lang) {
+  const selectedTranslations = translations[lang];
+
+  // Update nav + mobile nav
+
+  function updateElementsByID(id, text) {
+    const elements = document.querySelectorAll(`#${id}`);
+    elements.forEach(element => {
+      element.textContent = text;
+    });
+  }
+
+  updateElementsByID('projects', selectedTranslations.nav.projects);
+  updateElementsByID('about', selectedTranslations.nav.about);
+  updateElementsByID('contact', selectedTranslations.nav.contact);
+
+  //Update lang selection element
+
+  document.querySelector('.selected-lang .lang-text').textContent =
+    selectedTranslations.nav.selected;
+  document.querySelector(
+    '.selected-flag'
+  ).src = `https://flagsapi.com/${selectedTranslations.nav.flag}/shiny/32.png`;
+  // Update banner
+  document.getElementById('hi').textContent = selectedTranslations.banner.hi;
+  document.querySelector('.banner-content h1').textContent =
+    selectedTranslations.banner.name;
+  document.getElementById('job').textContent = selectedTranslations.banner.job;
+
+  // Update project section
+  document.getElementById('projectstitle').textContent =
+    selectedTranslations.projectsection.projectstitle;
+  document.getElementById('description1').innerHTML =
+    selectedTranslations.projectsection.description1;
+  document.getElementById('description2').innerHTML =
+    selectedTranslations.projectsection.description2;
+  document.getElementById('description3').innerHTML =
+    selectedTranslations.projectsection.description3;
+
+  // Update about section
+  document.getElementById('abouttitle').textContent =
+    selectedTranslations.aboutsection.abouttitle;
+  document.getElementById('abouttext').innerHTML =
+    selectedTranslations.aboutsection.abouttext;
+
+  // Update contact section
+  document.getElementById('contact-section').textContent =
+    selectedTranslations.contactsection.contacttitle;
+}
+
 // ---- MAP ---- //
 
-const map = new ol.Map({
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.TileJSON({
-        url: 'https://api.maptiler.com/maps/dataviz/tiles.json?key=UKzOdxVnuu8ffayB8YTM',
-        tileSize: 512,
-      }),
-    }),
-  ],
-  target: 'map',
-  view: new ol.View({
-    center: ol.proj.fromLonLat([-6.041013522970301, 54.551586227024686]),
-    zoom: 10.5,
-  }),
-});
+// const map = new ol.Map({
+//   layers: [
+//     new ol.layer.Tile({
+//       source: new ol.source.TileJSON({
+//         url: 'https://api.maptiler.com/maps/dataviz/tiles.json?key=UKzOdxVnuu8ffayB8YTM',
+//         tileSize: 512,
+//       }),
+//     }),
+//   ],
+//   target: 'map',
+//   view: new ol.View({
+//     center: ol.proj.fromLonLat([-6.041013522970301, 54.551586227024686]),
+//     zoom: 10.5,
+//   }),
+// });
 
-const marker = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    features: [
-      new ol.Feature({
-        geometry: new ol.geom.Point(
-          ol.proj.fromLonLat([-6.041013522970301, 54.551586227024686])
-        ),
-      }),
-    ],
-  }),
-  style: new ol.style.Style({
-    image: new ol.style.Icon({
-      src: './img/memoji-smile.png',
-      anchor: [0.5, 0.5],
-      scale: 0.2,
-    }),
-  }),
-});
+// const marker = new ol.layer.Vector({
+//   source: new ol.source.Vector({
+//     features: [
+//       new ol.Feature({
+//         geometry: new ol.geom.Point(
+//           ol.proj.fromLonLat([-6.041013522970301, 54.551586227024686])
+//         ),
+//       }),
+//     ],
+//   }),
+//   style: new ol.style.Style({
+//     image: new ol.style.Icon({
+//       src: './img/memoji-smile.png',
+//       anchor: [0.5, 0.5],
+//       scale: 0.2,
+//     }),
+//   }),
+// });
 
-map.addLayer(marker);
+// map.addLayer(marker);
